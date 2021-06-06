1b497eccab55bb882e1285a87304c429e65d40d6
yuc <732298225@qq.com>
2021-06-07 05:16:50 +0800

    💡程序生命周期初步

---
+++ src/main/java/cn/yuc/rest/demo/Application.java
@@ -0,0 +1,42 @@
+package cn.yuc.rest.demo;
+
+import cn.yuc.rest.demo.bean.BeanFactory;
+import cn.yuc.rest.demo.bean.impl.DefaultBeanFactory;
+import cn.yuc.rest.demo.web.router.impl.UserDefineRoutes;
+import cn.yuc.rest.demo.web.verticle.HttpServerVerticle;
+
+public class Application {
+
+    private static BeanFactory beanFactory;
+    private static UserDefineRoutes userDefineRoutes;
+
+    static {
+        try {
+            beanFactory = new DefaultBeanFactory();
+            userDefineRoutes = beanFactory.getBean(UserDefineRoutes.class);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static BeanFactory getBeanFactory(){
+        return beanFactory;
+    }
+
+    public static void run() {
+        userDefineRoutes.getAllRoutes();
+        HttpServerVerticle.run();
+    }
+
+    public static void main(String[] args) throws ClassNotFoundException {
+//        ClassScanner cs = new DefaultClassScanner();
+//        List<String> packageNames = new ArrayList<>();
+//        ConfigLoader.loadConfig();
+//        packageNames.add(ProjectConfig.getString(ConfigEnum.ROOT_PACKAGE));
+//        ProjectConfig.getInstance().put(ConfigEnum.CLASS_LIST,cs.collectClasses(packageNames));
+//        UserDefineRoutes udr = new UserDefineRoutes();
+//        List<UserDefineRoutes> udrSubBean = udr.getAllSubBean(ProjectConfig.getList(ConfigEnum.CLASS_LIST, Class.class));
+//        udrSubBean.forEach(System.out::println);
+        run();
+    }
+}
